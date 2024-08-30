9e7aa039c104ba55c17594497b0d0ad79201ba2c
Paulo Cesar <paulcesardearaujopaulo@gmail.com>
2024-08-29 22:55:42 -0300

    subindo projeto

---
+++ src/Contato.js
@@ -0,0 +1,11 @@
+
+import './Contato.css'
+
+export default function Contato(){
+    return(
+        <div className='pagina-contato'>
+             <h2 className='titulo'>Contato</h2>
+        </div>
+       
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Contato from './Contato';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Contato/>
+  </React.StrictMode>
+);
